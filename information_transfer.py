caa74c251041a9c56d402771bc5469979b20b3fd
jiingjing <jingjingr1000@yahoo.co.uk>
2026-05-20 19:22:16 +0100

    feat: create class BioInfoTrans + method

---
+++ information_transfer.py
@@ -0,0 +1,27 @@
+class BioInfoTrans:
+    def __init__(self, template_strand, codon_df):
+        self.template_strand = template_strand  # str: DNA template strand
+        self.codon_df = codon_df  # dataframe: codon table
+
+        self.validate_template_strand()
+
+    def validate_template_strand(self):
+        # must be DNA, must have polarity
+
+        ## remove any spaces
+        template_strand = self.template_strand.replace(" ", "")
+
+        ## check polarity - should have '5 or 3' at ends
+        left_end = template_strand[:2]
+        right_end = template_strand[-2:]
+
+        if not set(["3'", "5'"]) == set([left_end, right_end]):
+            raise ValueError("Strand does not have correct polarity.")
+
+        self.template_polarity = [left_end, right_end]  # save just the polarity
+        self.bases = template_strand[2:-2]  # save just the bases
+
+        ## check the strand is DNA
+        DNA, _ = self.DNA_or_RNA(self.bases)
+        if not DNA:
+            raise ValueError("Strand is not DNA")
