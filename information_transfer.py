5bf7b79c4d9eca5fefc3ffe6b353976c63be7ced
jiingjing <ridgway.z.ridgway@gmail.com>
2026-05-20 19:24:52 +0100

    feat: added methods to check if seq is DNA/RNA, and to change between DNA + RNA

--- information_transfer.py
+++ information_transfer.py
@@ -25,3 +25,19 @@ class BioInfoTrans:
         DNA, _ = self.DNA_or_RNA(self.bases)
         if not DNA:
             raise ValueError("Strand is not DNA")
+
+    def DNA_or_RNA(self, bases):
+        DNA_bases = set("ATGC")
+        RNA_bases = set("AUGC")
+
+        valid_DNA = set(bases) <= DNA_bases
+        valid_RNA = set(bases) <= RNA_bases
+        return valid_DNA, valid_RNA
+
+    def DNA_to_RNA(self, bases):
+        # T -> U
+        return bases.replace("T", "U")
+
+    def RNA_to_DNA(self, bases):
+        # U -> T
+        return bases.replace("U", "T")
